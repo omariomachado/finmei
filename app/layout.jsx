c8f8e342a743029ec49d51a9423a9b312f7765d6
omariomachado <mariohenriquemachado20@gmail.com>
2025-10-07 13:22:36 -0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,8 @@
 import "./globals.css";
+
 export const metadata = {
   title: "FinMEI — Gestão Financeira Simplificada para MEIs",
-  description: "Controle suas finanças de forma simples e prática, sem planilhas complicadas.",
+  description: "Controle suas finanças de forma simples e prática.",
 };
 
 export default function RootLayout({ children }) {
